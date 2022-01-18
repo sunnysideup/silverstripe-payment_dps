d09e7143cb227de599be348031da449c3e54836f
Nicolaas <github@sunnysideup.co.nz>
2022-01-18 13:09:48 +1300

    PATCH: removing superfluous file

--- _config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// @author Nicolaas [at] sunnysideup.co.nz
-
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START payment_dps MODULE ----------------===================
-
-//===================---------------- END dpspxpaypayment MODULE ----------------===================
